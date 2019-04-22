b2a70de9c56724f3bb5171fa31713cab334ae9c2
xuxuyy <13596217285@163.com>
2019-04-22 22:13:36 +0800

    root_tk_4.22

--- new_tk.py
+++ new_tk.py
@@ -1,12 +1,15 @@
 from tkinter import *
 import random
-def tran():
+def train():
     main=Tk()
     main.geometry("400x400")
-    while ture :
-        fn=random(100)
 
-    first_number=Label(main,textvariable=fn)
-
+    fn=random.randint(1,100)
+    sn = random.randint(1, 100)
+    result = fn + sn
+    first_number=Label(main,text=str(fn)).pack()
+    second_number = Label(main, text=str(sn)).pack()
+    aws=Entry(main)
+    print(str(result))
     main.mainloop()
-a=tran()
+train()
--- root.py
+++ root.py
@@ -1,22 +1,23 @@
 from tkinter import *
-
+from new_tk import *
 global top,r
-def jumpe(a):
-    if a == "1":
-        top.exit()
-        test= Tk()
-        nu1= Label(test,text="1").pack()
+# def jumpe(a):
+#     if a == "1":
+#         top.exit()
+#         test= Tk()
+#         nu1= Label(test,text="1").pack()
 
 def add_1():
-    change .set(r.get())
+    change =r.get()
     if change == '1':
-        tran=tran()
+        train()
     elif change == '2':
-        test=test()
+        test()
     elif change == '3':
-        gogo=gogo()
+        gogo()
 change="1"
 top = Tk()
+top.geometry("200x300")
 r = StringVar()
 change = StringVar()
 r.set('0')
@@ -28,9 +29,9 @@ model=Radiobutton(top,text='口算训练', variable=r, value='1').pack()
 mode1=Radiobutton(top,text='口算测验', variable=r, value='2').pack()
 mode1=Radiobutton(top,text='无尽模式', variable=r, value='3').pack()
 
-#button1 = Button(top,text="开始练习",command=jumpe(r.get()))
+button1 = Button(top,text="退出",command=quit)
 button2 = Button(top,text='开始',command = add_1)
 
-#button1.pack(side=LEFT)
-button2.pack(side=RIGHT)
+button1.pack(side=RIGHT)
+button2.pack(side=LEFT)
 top.mainloop()
