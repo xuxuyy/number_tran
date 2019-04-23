57f3c0174c0ec2a0110b440545e10c64cd9119d9
xuxuyy <13596217285@163.com>
2019-04-23 22:46:34 +0800

    root_tk_4.23

--- new_tk.py
+++ new_tk.py
@@ -1,15 +1,32 @@
 from tkinter import *
 import random
+
+
+
+def aws_fun():
+    if str(result) == aws_vlaue.get():
+        show='回答正确'
+    else:
+        show='回答错误，正确答案是'+ str(result)
+    aws_show=Label(main,text=show).pack()
+    # print(aws_vlaue.get())
 def train():
+    global main
     main=Tk()
+    global aws_vlaue,result
+    aws_vlaue=StringVar()
     main.geometry("400x400")
 
-    fn=random.randint(1,100)
+    fn = random.randint(1,100)
     sn = random.randint(1, 100)
     result = fn + sn
     first_number=Label(main,text=str(fn)).pack()
     second_number = Label(main, text=str(sn)).pack()
-    aws=Entry(main)
+    aws=Entry(main,state="normal",textvariable=aws_vlaue).pack()
+    aws_button=Button(main,text="正确答案",command=aws_fun).pack()
+    next_button=Button(main,text="下一题").pack()
+
+
     print(str(result))
     main.mainloop()
 train()
