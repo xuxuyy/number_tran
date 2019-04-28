70babce515e2923624ef33b3d891c987bc9e8902
xuxuyy <13596217285@163.com>
2019-04-28 23:10:33 +0800

    root_tk_4.28

--- new_tk.py
+++ new_tk.py
@@ -1,32 +1,42 @@
 from tkinter import *
 import random
 
-
-
 def aws_fun():
+    global show
+
     if str(result) == aws_vlaue.get():
-        show='回答正确'
+        show.set('回答正确')
     else:
-        show='回答错误，正确答案是'+ str(result)
-    aws_show=Label(main,text=show).pack()
+        show.set('回答错误，正确答案是'+ str(result))
+    aws_show=Label(main,textvariable=show).pack()
     # print(aws_vlaue.get())
+def aws_flush():
+    global result
+    fn.set(random.randint(1, 100))
+    sn.set(random.randint(1, 100))
+    result = fn.get() + sn.get()
+    show.set(" ")
+
+
 def train():
     global main
     main=Tk()
-    global aws_vlaue,result
+    global aws_vlaue,result,fn,sn,show
+    show = StringVar()
     aws_vlaue=StringVar()
-    main.geometry("400x400")
+    fn=IntVar()
+    sn = IntVar()
 
-    fn = random.randint(1,100)
-    sn = random.randint(1, 100)
-    result = fn + sn
-    first_number=Label(main,text=str(fn)).pack()
-    second_number = Label(main, text=str(sn)).pack()
+    main.geometry("400x400")
+    aws_flush()
+
+    first_number=Label(main,textvariable=fn).pack()
+    second_number = Label(main, textvariable=sn).pack()
     aws=Entry(main,state="normal",textvariable=aws_vlaue).pack()
     aws_button=Button(main,text="正确答案",command=aws_fun).pack()
-    next_button=Button(main,text="下一题").pack()
+    next_button=Button(main,text="下一题",command=aws_flush).pack()
 
 
-    print(str(result))
+
     main.mainloop()
 train()
