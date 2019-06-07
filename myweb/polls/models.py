0cc2800bcff6f51deffea92c8132c024e94df08c
xuxuyy <13596217285@163.com>
2019-06-08 00:39:54 +0800

    database_first_create

--- myweb/polls/models.py
+++ myweb/polls/models.py
@@ -4,4 +4,17 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    ticket_number = models.IntegerField()
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
