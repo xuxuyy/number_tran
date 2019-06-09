163a887373143516502e183602ca3b7912b493a0
xuxuyy <13596217285@163.com>
2019-06-09 13:44:53 +0800

    page_create

--- myweb/polls/models.py
+++ myweb/polls/models.py
@@ -8,7 +8,6 @@ class Person(models.Model):
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-
     def __str__(self):
         return self.question_text
 
